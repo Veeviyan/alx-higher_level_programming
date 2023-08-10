ef98958b89159b963b6888f3d598892f2637e470
Veeviyan <snowygeenie@gmail.com>
2023-08-10 13:52:59 -0700

    Solution to task 5

---
+++ 0x01-python-if_else_loops_functions/5-print_comb2.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+# 5-print_comb2.py
+
+# Vivian Okaforcha <snowygeenie@gmail.com>
+
+
+for i in range(100):
+    if i < 99:
+        print("{:02}, ".format(i), end="")
+    else:
+        print(i)
