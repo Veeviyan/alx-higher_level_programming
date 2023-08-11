a700a3ab346228af3ef84f82228a3d1f4aa3274d
Veeviyan <snowygeenie@gmail.com>
2023-08-11 08:39:27 -0700

    Fixed import execution error

--- 0x02-python-import_modules/0-add.py
+++ 0x02-python-import_modules/0-add.py
@@ -3,7 +3,8 @@
 
 from add_0 import add
 
-a = 1
-b = 2
-result = add(a, b)
-print(f"{a:d} + {b:d} = {result:d}".format(a, b, result))
+if __name__ == "__main__":
+    a = 1
+    b = 2
+    result = add(a, b)
+    print(f"{a:d} + {b:d} = {result:d}".format(a, b, result))
