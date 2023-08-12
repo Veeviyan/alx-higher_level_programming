cb5a6bc533b9e9c529f44998a1bd18db4ef837b0
Veeviyan <snowygeenie@gmail.com>
2023-08-11 21:34:59 -0700

    Solution to task 3

--- 0x02-python-import_modules/3-infinite_add.py
+++ 0x02-python-import_modules/3-infinite_add.py
@@ -3,18 +3,8 @@
 from sys import argv
 
 if __name__ == "__main__":
-    def main():
-        n = len(argv)
-        print(f"Total arguments passed:".format(n))
 
-        print(f"\nName of script:".format(argv[0]))
-
-        print(f"\nArguments passed:", end="")
-        for i in range(1, n):
-            print(argv[i], end="")
-
-        Sum = 0
-        for i in range(1, n):
-            Sum = Sum + int(argv[i])
-
-        print(f"\n\nResult:".format(Sum))
+    sum = 0
+    for i in range(len(argv) - 1):
+        sum += int(argv[i + 1])
+    print("{}".format(sum))
