bb373fe365c708b9ae54adac480119be56569164
Veeviyan <snowygeenie@gmail.com>
2023-08-14 04:15:47 -0700

    Fixed output error

--- 0x03-python-data_structures/3-print_reversed_list_integer.py
+++ 0x03-python-data_structures/3-print_reversed_list_integer.py
@@ -1,6 +1,8 @@
 #!/usr/bin/python3
 
 def print_reversed_list_integer(my_list=[]):
-    reversed_list = my_list[::-1]
+    i = len(my_list)
+    reversed_list = my_list[i - 1::-1]
+
     for num in reversed_list:
         print("{:d}".format(num))
