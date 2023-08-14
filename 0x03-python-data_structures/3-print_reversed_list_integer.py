1917e54cf820e0507fbdf04919da897293b06a57
Veeviyan <snowygeenie@gmail.com>
2023-08-14 04:35:47 -0700

    Fixed output error

--- 0x03-python-data_structures/3-print_reversed_list_integer.py
+++ 0x03-python-data_structures/3-print_reversed_list_integer.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 
 def print_reversed_list_integer(my_list=[]):
-    if my_list != None:
+    if my_list is not None:
         i = len(my_list)
         reversed_list = my_list[i - 1::-1]
 
