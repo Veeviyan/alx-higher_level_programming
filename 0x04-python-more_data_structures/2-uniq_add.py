e93b1bb1123572df2cd47477a693e46d3385afc1
Veeviyan <snowygeenie@gmail.com>
2023-08-17 02:18:19 -0700

    fixed error

--- 0x04-python-more_data_structures/2-uniq_add.py
+++ 0x04-python-more_data_structures/2-uniq_add.py
@@ -1,11 +1,8 @@
 #!/usr/bin/python3
 
 def uniq_add(my_list=[]):
-    unique_int = []
     result = 0
 
-    if my_list:
-        for i in set(my_list):
-            if i not in unique_int:
-                result = i + result
-        return (result)
+    for i in set(my_list):
+        result = i + result
+    return (result)
